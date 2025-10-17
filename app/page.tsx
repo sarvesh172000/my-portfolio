00717d9a84ee1e700546933e468c55c9f77bcaf1
sarvesh172000 <sarvesh.waghmare101@gmail.com>
2025-10-17 15:26:59 -0700

    descrition change

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   const firstName = "Sarvesh".split("");
   const lastName = "Waghmare".split("");
 
-  const roles = ["Data Engineer", "Software Developer",  "ML Engineer", "Data Analyst"];
+  const roles = [ "AI/ML Engineer", "Data Engineer", "Software Developer"];
   const [currentRoleIndex, setCurrentRoleIndex] = useState(0);
 
   useEffect(() => {
