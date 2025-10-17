64ee8205b44422316b07dbdfe073b8dd6f31f58f
sarvesh172000 <sarvesh.waghmare101@gmail.com>
2025-10-17 15:24:05 -0700

    resume url change

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -5,7 +5,7 @@ import { FaDownload } from 'react-icons/fa';
 
 export default function ResumePage() {
   // Google Drive file ID from your link
-  const fileId = "1T7rBYZfsqiouliVYZ98SSmDekhSubYDz";
+  const fileId = "1cgk0xkGASWkOxEk5fzRcMx9adU7MBgYa";
   
   // Construct the direct download and embed URLs
   const downloadLink = `https://drive.google.com/uc?export=download&id=${fileId}`;
